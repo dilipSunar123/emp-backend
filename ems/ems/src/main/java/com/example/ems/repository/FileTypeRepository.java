21e39a1c73d1cdc50ff6b1ca4b416de200173bd3
Dilip <71514397+dilipSunar123@users.noreply.github.com>
2024-01-02 11:29:54 +0530

    Delete ems/ems directory

--- ems/ems/src/main/java/com/example/ems/repository/FileTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ems.repository;
-
-import com.example.ems.entity.FileTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileTypeRepository extends JpaRepository<FileTypeEntity, Integer> {
-}
